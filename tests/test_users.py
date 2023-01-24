8fa4b679f4231d4deff0ac8d01e31c9849e4500a
Anant0810 <sadhu.anant@gmail.com>
2023-01-25 01:36:38 +0530

    added postgres services

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,10 +3,10 @@ import pytest
 from app import schemas
 from app.config import settings
 
-def test_root(client):
-    res = client.get("/")
-    assert res.json().get('message') == "Hello World!!!"
-    assert res.status_code == 200
+# def test_root(client):
+#     res = client.get("/")
+#     assert res.json().get('message') == "Hello World!!!"
+#     assert res.status_code == 200
 
 def test_create_user(client):
     res = client.post("/users/", json={"email" : "hello1234@gmail.com", "password": "password123"})
